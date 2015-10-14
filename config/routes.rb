e56906269719218423f37b21961a1dcab2ed5baa
Jayson Grace <jayson.e.grace@gmail.com>
2015-10-14 17:22:37 -0600

    Change root based on whether or not the user is signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-
   devise_for :users
   get 'login/index'
-  root 'login#index'
+  resources :transactions
 
+  authenticated :user do
+    root 'transactions#index', as: "authenticated_root"
+  end
+
+  root 'login#index'
   resources :costs
   resources :incomes
   resources :categories
-  resources :transactions
 end
