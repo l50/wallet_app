817db0664e168ca21686355c8ca8ec713be2e62e
Jayson Grace <jayson.e.grace@gmail.com>
2015-10-29 22:41:12 -0600

    Everything working properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create a lot of Categories
+# 1.upto(50) { Category.create({name: "Drinks"}) }
+
+# Create a lot of Transactions
+1.upto(50) { Transaction.create({date: "2015-10-30 02:23:00", description: "Fucks", category_id: 1, amount: 12, transaction_type: 1, user_id: 1}) }
+
