26d31362639c8761877968e85825434ce05daccd
Jayson Grace <jayson.e.grace@gmail.com>
2015-11-01 16:42:23 -0700

    Created seeds and updated show interface for transactions to link to category

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Create a lot of Categories
-# 1.upto(50) { Category.create({name: "Drinks"}) }
+# Create Categories
+Category.create({name: "Food"})
+Category.create({name: "Work Payment"})
 
-# Create a lot of Transactions
-1.upto(50) { Transaction.create({date: "2015-10-30 02:23:00", description: "Transaction", category_id: 1, amount: 12,
-transaction_type: 1, user_id: 1}) }
+spender = Category.where(name: 'Food')
+maker   = Category.where(name: 'Work Payment')
+# Create Transactions
+1.upto(2) { Transaction.create({date: "2015-10-30 02:23:00", description: "Spending Money", category_id: spender.ids.first, amount: 100,
+                                transaction_type: 0, user_id: 1}) }
+1.upto(2) { Transaction.create({date: "2015-10-30 02:23:00", description: "Making Money", category_id: maker.ids
+                                                                                                           .first, amount: 50, transaction_type: 1, user_id: 1}) }
 
