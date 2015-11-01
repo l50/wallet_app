fe45223b4992e3727ad3e63b02670fd853baf1ac
Jayson Grace <jayson.e.grace@gmail.com>
2015-10-31 23:31:23 -0600

    Add model level validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,5 @@ class Transaction < ActiveRecord::Base
   enum transaction_type: [:cost, :income]
   belongs_to :category
   belongs_to :user
+  validates_presence_of :description, :amount, :category_id
 end
