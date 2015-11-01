b490147b2ee804413f93448e0a7d932198eeecab
Jayson Grace <jayson.e.grace@gmail.com>
2015-11-01 16:24:05 -0700

    Fixed validations with selection of cost/income

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 class Transaction < ActiveRecord::Base
-  enum transaction_type: [:cost, :income]
+  TRANSACTION_TYPES = [:cost, :income]
+  enum transaction_type: TRANSACTION_TYPES
   belongs_to :category
   belongs_to :user
   validates_presence_of :description, :amount, :category_id
+  validates :transaction_type, inclusion: { in: TRANSACTION_TYPES.map {|t| t.to_s } }
 end
